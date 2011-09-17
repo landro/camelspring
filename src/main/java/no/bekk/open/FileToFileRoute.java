b266cc1789ebb9890c06d5b2f87142f4dc420faf
Stefan Magnus Landrø <stefan.landro@gmail.com>
2011-09-17 23:40:25 +0200

    Initial commit

---
+++ src/main/java/no/bekk/open/FileToFileRoute.java
@@ -0,0 +1,14 @@
+package no.bekk.open;
+
+import org.apache.camel.spring.SpringRouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileToFileRoute extends SpringRouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("file:data/inbox").to("file:data/outbox");
+	}
+
+}
---
+++ src/main/java/no/bekk/open/Main.java
@@ -0,0 +1,17 @@
+package no.bekk.open;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new ClassPathXmlApplicationContext("beans.xml");
+		
+		while (true) {
+
+		}
+
+	}
+
+}
